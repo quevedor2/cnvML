4af9ae9ac37fed9bdc7575768b736af33f1c95b1
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-05-24 07:22:36 -0400

    - set a "seed"  for randomization of the Hilbert data, also added "Normals" stemming from 1000G Corriell dataset (n=200) - Hilbert TCGA now ranges from black (0,0,0) to red or blue (255,0,0) or (0,0,255)

--- preprocessing/hilbert_tcga.R
+++ preprocessing/hilbert_tcga.R
@@ -32,15 +32,30 @@ addCumPos <- function(dat, ref, dat.type){
 
 ##############
 #### Main ####
-PDIR <- '/mnt/work1/users/pughlab/projects/cancer_cell_lines/TCGA'
-segf <- file.path(PDIR, "input", "TCGA_mastercalls.abs_segtabs.fixed.txt")
+PDIR <- '/mnt/work1/users/pughlab/projects/cancer_cell_lines'
+segf <- file.path(PDIR, "TCGA", "input", "TCGA_mastercalls.abs_segtabs.fixed.txt")
 segd <- read.table(segf, sep="\t", header=TRUE, stringsAsFactors = FALSE)
 colnames(segd)[2:4] <- c('chrom', 'loc.start', 'loc.end')
+
+normal_segf <- file.path(PDIR, '1000G', 'eacon', 'symlinks', 'seg', '1000g.seg')
+normal_segd <- read.table(normal_segf, sep="\t", header=TRUE, stringsAsFactors = FALSE)
+colnames(normal_segd) <- c('Sample', 'chrom', 'Chrom', 'loc.start', 'loc.end', 
+                           'Length', 'Modal_Total_CN', 'Modal_HSCN_1', 'Modal_HSCN_2')
+segd2 <- plyr::rbind.fill(segd, normal_segd)
+#segd <- segd2[grep("^HG", segd2$Sample),]
+segd <- segd2
 
 chr.size.dat <- getChrLength()
 seqlevelsStyle(chr.size.dat) <- 'NCBI'
 scale <- 1e5
 order=7
+a_col=c("black", "red")
+b_col=c("black", "blue")
+breaks <- seq(0, 5, by=0.1)
+#col2rgb(colorRampPalette(a_col)(length(breaks)))
+#col2rgb(colorRampPalette(b_col)(length(breaks)))
+
+
 
 segl <- split(segd, segd$Sample)
 hilberts <- lapply(segl, function(seg){
@@ -55,16 +70,19 @@ hilberts <- lapply(segl, function(seg){
     ir <- IRanges(chr.data$cloc.start/scale, chr.data$cloc.end/scale)
     
     ## Plot Allele specific copy-number
-    png(file.path(PDIR, "output", "hilbert", paste0(sample_id, ".png")), 
+    png(file.path(PDIR, "TCGA", "output", "hilbert", paste0(sample_id, ".png")), 
         width=300, height=300)
     hc = HilbertCurve(1, max(chr.size.dat$cum.end)/scale, level = order, mode = "pixel", 
                       reference = TRUE) # title = basename(segf), 
-    breaks <- seq(0, 5, by=0.1)
     a1 = circlize::colorRamp2(breaks = breaks,
-                              colors = colorRampPalette(c("white", "red", "darkred"))(length(breaks)))
+                              colors = colorRampPalette(a_col)(length(breaks)))
     a2 = circlize::colorRamp2(breaks = breaks,
-                              colors = colorRampPalette(c("white", "blue", "darkblue"))(length(breaks)))
+                              colors = colorRampPalette(b_col)(length(breaks)))
+    #cols <- DescTools::MixColor(col1 = a1(breaks), col2 = a2(breaks))
+    # Ensure that Red/Blue channels range from 0-255. MixColor halves both values when mixing
     cols <- DescTools::MixColor(col1 = a1(chr.data$Modal_HSCN_1), col2 = a2(chr.data$Modal_HSCN_2))
+    cols <- apply(col2rgb(cols),2,function(x) rgb(x[1]*2, x[2]*2, x[3]*2, maxColorValue = 255))
+    
     hc_layer(hc, ir, mean_mode = "absolute", col = cols)
     hc_polygon(hc, x1=chr.size.dat$cum.start/scale, x2=chr.size.dat$cum.end/scale)
     hc_text(hc, x1=chr.size.dat$cum.start/scale, x2=chr.size.dat$cum.end/scale, 
