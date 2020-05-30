e9910d68b1edbb147caa75f0420fb972011cdbc5
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-05-30 11:42:33 -0400

    Added compatibility of generating Hilbert plots with CCL files

--- preprocessing/hilbert_tcga.R
+++ preprocessing/hilbert_tcga.R
@@ -32,64 +32,87 @@ addCumPos <- function(dat, ref, dat.type){
 
 ##############
 #### Main ####
+analysis <- 'ccl_aggregate'
 PDIR <- '/mnt/work1/users/pughlab/projects/cancer_cell_lines'
-segf <- file.path(PDIR, "TCGA", "input", "TCGA_mastercalls.abs_segtabs.fixed.txt")
-segd <- read.table(segf, sep="\t", header=TRUE, stringsAsFactors = FALSE)
-colnames(segd)[2:4] <- c('chrom', 'loc.start', 'loc.end')
+if(analysis=='TCGA'){
+  seg_files <- "TCGA_mastercalls.abs_segtabs.fixed.txt"
+} else if(analysis=='ccl_aggregate'){
+  seg_files <- c('CCLE_cna_hg19.seg', 'GDSC_cna_hg19.seg', 'gCSI_cna_hg19.seg')
+} else {
+  stop("Analysis must be either 'TCGA' or 'ccl_aggregate'")
+}
 
-normal_segf <- file.path(PDIR, '1000G', 'eacon', 'symlinks', 'seg', '1000g.seg')
-normal_segd <- read.table(normal_segf, sep="\t", header=TRUE, stringsAsFactors = FALSE)
-colnames(normal_segd) <- c('Sample', 'chrom', 'Chrom', 'loc.start', 'loc.end', 
-                           'Length', 'Modal_Total_CN', 'Modal_HSCN_1', 'Modal_HSCN_2')
-segd2 <- plyr::rbind.fill(segd, normal_segd)
-#segd <- segd2[grep("^HG", segd2$Sample),]
-segd <- segd2
-
-chr.size.dat <- getChrLength()
-seqlevelsStyle(chr.size.dat) <- 'NCBI'
-scale <- 1e5
-order=7
-a_col=c("black", "red")
-b_col=c("black", "blue")
-breaks <- seq(0, 5, by=0.1)
-#col2rgb(colorRampPalette(a_col)(length(breaks)))
-#col2rgb(colorRampPalette(b_col)(length(breaks)))
-
-
-
-segl <- split(segd, segd$Sample)
-hilberts <- lapply(segl, function(seg){
-  sample_id <- unique(seg$Sample) 
-  print(paste0("Sample: ", sample_id, " (", grep(sample_id, names(segl)), 
-               "/", length(segl), ")"))
-  
-  if(!all(is.na(seg$chrom))){
-    seg$chrom <- gsub("23", "X", seg$chrom) 
-    seg$chrom <- gsub("24", "Y", seg$chrom) 
-    chr.data <- addCumPos(seg, chr.size.dat, dat.type='seg')
-    ir <- IRanges(chr.data$cloc.start/scale, chr.data$cloc.end/scale)
+for(seg_i in seg_files){
+  segf <- file.path(PDIR, analysis, "input", seg_i)
+  segd <- read.table(segf, sep="\t", header=TRUE, stringsAsFactors = FALSE)
+  if(analysis=='TCGA'){
+    colnames(segd)[2:4] <- c('chrom', 'loc.start', 'loc.end')
     
-    ## Plot Allele specific copy-number
-    png(file.path(PDIR, "TCGA", "output", "hilbert", paste0(sample_id, ".png")), 
-        width=300, height=300)
-    hc = HilbertCurve(1, max(chr.size.dat$cum.end)/scale, level = order, mode = "pixel", 
-                      reference = TRUE) # title = basename(segf), 
-    a1 = circlize::colorRamp2(breaks = breaks,
-                              colors = colorRampPalette(a_col)(length(breaks)))
-    a2 = circlize::colorRamp2(breaks = breaks,
-                              colors = colorRampPalette(b_col)(length(breaks)))
-    #cols <- DescTools::MixColor(col1 = a1(breaks), col2 = a2(breaks))
-    # Ensure that Red/Blue channels range from 0-255. MixColor halves both values when mixing
-    cols <- DescTools::MixColor(col1 = a1(chr.data$Modal_HSCN_1), col2 = a2(chr.data$Modal_HSCN_2))
-    cols <- apply(col2rgb(cols),2,function(x) rgb(x[1]*2, x[2]*2, x[3]*2, maxColorValue = 255))
-    
-    hc_layer(hc, ir, mean_mode = "absolute", col = cols)
-    hc_polygon(hc, x1=chr.size.dat$cum.start/scale, x2=chr.size.dat$cum.end/scale)
-    hc_text(hc, x1=chr.size.dat$cum.start/scale, x2=chr.size.dat$cum.end/scale, 
-            labels = c(1:22, "X", "Y"), gp = gpar(fontsize = 10), 
-            centered_by = "polygon")
-    dev.off()
-    
+    normal_segf <- file.path(PDIR, '1000G', 'eacon', 'symlinks', 'seg', '1000g.seg')
+    normal_segd <- read.table(normal_segf, sep="\t", header=TRUE, stringsAsFactors = FALSE)
+    colnames(normal_segd) <- c('Sample', 'chrom', 'Chrom', 'loc.start', 'loc.end', 
+                               'Length', 'Modal_Total_CN', 'Modal_HSCN_1', 'Modal_HSCN_2')
+    segd2 <- plyr::rbind.fill(segd, normal_segd)
+    #segd <- segd2[grep("^HG", segd2$Sample),]
+    segd <- segd2
+  } else {
+    colnames(segd)[c(1:3,6,11,7,8)] <- c('chrom', 'loc.start', 'loc.end',
+                                  'Sample', 'Modal_Total_CN', 
+                                  'Modal_HSCN_1', 'Modal_HSCN_2')
   }
-})
+  
+  
+  chr.size.dat <- getChrLength()
+  seqlevelsStyle(chr.size.dat) <- 'NCBI'
+  scale <- 1e5
+  order=7
+  a_col=c("black", "red")
+  b_col=c("black", "blue")
+  breaks <- seq(0, 5, by=0.1)
+  #col2rgb(colorRampPalette(a_col)(length(breaks)))
+  #col2rgb(colorRampPalette(b_col)(length(breaks)))
+  
+  segl <- split(segd, segd$Sample)
+  
+  hilberts <- lapply(segl, function(seg){
+    sample_id <- unique(seg$Sample) 
+    print(paste0(seg_i, "-Sample: ", sample_id, " (", 
+                 grep(sample_id, names(segl)), "/", length(segl), ")"))
+    
+    if(!all(is.na(seg$chrom))){
+      seg$chrom <- gsub("23", "X", seg$chrom) 
+      seg$chrom <- gsub("24", "Y", seg$chrom) 
+      seg$chrom <- gsub("^chr", "", seg$chrom, ignore.case = TRUE)
+      chr.data <- addCumPos(seg, chr.size.dat, dat.type='seg')
+      ir <- IRanges(chr.data$cloc.start/scale, chr.data$cloc.end/scale)
+      
+      ## Plot Allele specific copy-number
+      png(file.path(PDIR, analysis, "output", "hilbert", paste0(sample_id, ".png")), 
+          width=300, height=300)
+      hc = HilbertCurve(1, max(chr.size.dat$cum.end)/scale, level = order, mode = "pixel", 
+                        reference = TRUE) # title = basename(segf), 
+      a1 = circlize::colorRamp2(breaks = breaks,
+                                colors = colorRampPalette(a_col)(length(breaks)))
+      a2 = circlize::colorRamp2(breaks = breaks,
+                                colors = colorRampPalette(b_col)(length(breaks)))
+      #cols <- DescTools::MixColor(col1 = a1(breaks), col2 = a2(breaks))
+      # Ensure that Red/Blue channels range from 0-255. MixColor halves both values when mixing
+      cols <- DescTools::MixColor(col1 = a1(chr.data$Modal_HSCN_1), col2 = a2(chr.data$Modal_HSCN_2))
+      cols <- apply(col2rgb(cols),2,function(x) rgb(x[1]*2, x[2]*2, x[3]*2, maxColorValue = 255))
+      
+      hc_layer(hc, ir, mean_mode = "absolute", col = cols)
+      hc_polygon(hc, x1=chr.size.dat$cum.start/scale, x2=chr.size.dat$cum.end/scale)
+      hc_text(hc, x1=chr.size.dat$cum.start/scale, x2=chr.size.dat$cum.end/scale, 
+              labels = c(1:22, "X", "Y"), gp = gpar(fontsize = 10), 
+              centered_by = "polygon")
+      dev.off()
+      
+    }
+  })
+}
+
+
+
+
+
 
