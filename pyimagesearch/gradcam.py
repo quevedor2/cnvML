bbdc032e57add0f52a40cac6ee6a856fec34d702
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-07-09 22:02:44 -0400

    minor tweak to grad cam to return the cam values rather than normalized heat map values

--- pyimagesearch/gradcam.py
+++ pyimagesearch/gradcam.py
@@ -86,6 +86,52 @@ class GradCAM:
 		# return the resulting heatmap to the calling function
 		return heatmap
 	
+	def compute_heatmap_raw(self, image, eps=1e-8):
+		# construct our gradient model by supplying (1) the inputs
+		# to our pre-trained model, (2) the output of the (presumably)
+		# final 4D layer in the network, and (3) the output of the
+		# softmax activations from the model
+		gradModel = Model(
+			inputs=[self.model.inputs],
+			outputs=[self.model.get_layer(self.layerName).output,
+				self.model.output])
+		
+		# record operations for automatic differentiation
+		with tf.GradientTape() as tape:
+			# cast the image tensor to a float-32 data type, pass the
+			# image through the gradient model, and grab the loss
+			# associated with the specific class index
+			inputs = tf.cast(image, tf.float32)
+			(convOutputs, predictions) = gradModel(inputs)
+			loss = predictions[:, self.classIdx]
+		
+		# use automatic differentiation to compute the gradients
+		grads = tape.gradient(loss, convOutputs)
+		
+		# compute the guided gradients
+		castConvOutputs = tf.cast(convOutputs > 0, "float32")
+		castGrads = tf.cast(grads > 0, "float32")
+		guidedGrads = castConvOutputs * castGrads * grads
+		
+		# the convolution and guided gradients have a batch dimension
+		# (which we don't need) so let's grab the volume itself and
+		# discard the batch
+		convOutputs = convOutputs[0]
+		guidedGrads = guidedGrads[0]
+		
+		# compute the average of the gradient values, and using them
+		# as weights, compute the ponderation of the filters with
+		# respect to the weights
+		weights = tf.reduce_mean(guidedGrads, axis=(0, 1))
+		cam = tf.reduce_sum(tf.multiply(weights, convOutputs), axis=-1)
+		
+		# grab the spatial dimensions of the input image and resize
+		# the output class activation map to match the input image
+		# dimensions
+		(w, h) = (image.shape[2], image.shape[1])
+		heatmap = cv2.resize(cam.numpy(), (w, h))
+		return(cam, heatmap)
+	
 	def overlay_heatmap(self, heatmap, image, alpha=0.5,
 		colormap=cv2.COLORMAP_VIRIDIS):
 		# apply the supplied color map to the heatmap and then
