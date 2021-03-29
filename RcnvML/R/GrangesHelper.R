9be1bfb43a927762a2507464a90429cdefb2f053
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-03-29 15:03:45 -0400

    Function intersectAndAnnotate to intersect two Granges and carry over the mcol data

--- RcnvML/R/GrangesHelper.R
+++ RcnvML/R/GrangesHelper.R
@@ -7,6 +7,7 @@
 #' GRanges object with the cumulative postiions added to it
 #' @importFrom GenomeInfoDb seqlevelsStyle
 #' @importFrom GenomeInfoDb seqnames
+#' @importFrom methods as
 #' @export
 calcCumulativeGPos <- function(gr0, seqstyle='ENSEMBL'){
   chrs <- getChrLength()
@@ -22,4 +23,32 @@ calcCumulativeGPos <- function(gr0, seqstyle='ENSEMBL'){
     return(chr_gr)
   }), "GRangesList"))
   return(gr0)
+}
+
+#' Intersect and annotate sample with reference GR
+#' @description Intersects the sample GR (s_gr) with the 
+#' reference GR (ref_gr) and annotates all of the overlaps
+#' in the sample GR with the reference
+#' 
+#' @param s_gr Sample GenomicRanges object
+#' @param ref_gr Reference GenomicRanges object
+#' @param cpos cpos
+#'
+#' @return
+#' A GRanges intersected between s_gr and ref_gr with annotations
+#' from ref_gr
+#' @importFrom assertthat assert_that
+#' @import GenomicRanges
+#' @export
+intersectAndAnnotate <- function(s_gr, ref_gr, cpos=TRUE){
+  ## Intersects s_gr with ref_gr, then annotates all of s_gr component
+  ## on the new intersect
+  print(unique(s_gr$ID))
+  end(s_gr) <- end(s_gr)-1 # Prevents merging of adjacent segments
+  int_gr <- intersect(ref_gr, s_gr, ignore.strand=FALSE)
+  ov_idx <- findOverlaps(int_gr, s_gr)
+  mcols(int_gr) <- mcols(s_gr[subjectHits(ov_idx),])
+  if(cpos) int_gr <- calcCumulativeGPos(int_gr)
+  
+  return(int_gr)
 }
