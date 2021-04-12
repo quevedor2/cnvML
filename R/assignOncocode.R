5dce55ce3e562ec0e6b978bf4e7a82d8a60b5897
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-04-12 11:49:40 -0400

    Fixed the cancer cell line oncocode assignment, need to add in the TCGA assignment still

--- R/assignOncocode.R
+++ R/assignOncocode.R
@@ -29,11 +29,14 @@ option_list <- list(
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 
+## Load libraries
+libraries <- c('filesstrings', 'RcnvML', 'assertthat')
+null <- lapply(libraries, function(i) suppressMessages(require(i, character.only = TRUE)))
 # Set up parameters
-PDIR <- opt$pdir                   # Path to seg files [file.path(PDIR, analysis, input, seg_i)]
-analysis <- opt$dataset           # dataset to analyze (TCGA or ccl_aggregate)
-sfc <- opt$sfc                    # whether to use a hilbert SFC or sweep
-cntype <- opt$cntype              # Plot ASCN or TCN as colours
+PDIR <- opt$pdir          # Path to seg files [file.path(PDIR, analysis, input, seg_i)]
+analysis <- opt$dataset   # dataset to analyze (TCGA or ccl_aggregate)
+sfc <- opt$sfc            # whether to use a hilbert SFC or sweep
+cntype <- opt$cntype      # Plot ASCN or TCN as colours
 
 # Asserts
 assert_that(length(cntype)==1, any(c('TCN', 'ASCN') %in% cntype), 
@@ -45,38 +48,40 @@ assert_that(length(analysis)==1, any(c('ccl_aggregate', 'TCGA') %in% analysis),
 
 #### Main ####
 ##############
-library(filesstrings)
-library(RcnvML)
-
-setwd(file.path(pdir, analysis))
+setwd(file.path(PDIR, analysis))
 data("onco_meta_df")
-# meta_df <- readRDS(file.path(PDIR, "ref", "onco_meta_df.rds"))
 
 ## List all hilbert files
-fls <- list.files(file.path(PDIR, "output", "hilbert"), pattern="png$")
-datasets <- c('GDSC', 'CCLE', 'GNE')
-meta_df$GDSC <- gsub(".cel$", "", meta_df$GDSC, ignore.case = TRUE)
+fls <- list.files(file.path("output", sfc, cntype), pattern="png$")
+assert_that(length(fls) > 1, 
+            msg='Could not locate SFC png images outputed from plotSFC.R')
 
-copy_status <- sapply(fls, function(fl_i){
-  # Map file to the meta_df
-  print(fl_i)
-  i <- gsub(".png$", "", fl_i)
-  idx <- which(i == meta_df[,datasets], arr.ind=TRUE)
+if(analysis == 'ccl_aggregate'){
+  ## Assigns the CCL files to different oncocode directories
+  datasets <- c('GDSC', 'CCLE', 'GNE')
+  onco_meta_df$GDSC <- gsub(".cel$", "", onco_meta_df$GDSC, ignore.case = TRUE)
   
-  # Identity dataset and oncocode
-  ds <- datasets[idx[,2]]
-  oncocode <- as.character(gsub(":.*", "", meta_df[idx[,1],]$oncocode))
+  copy_status <- sapply(fls, function(fl_i){
+    # Map file to the onco_meta_df
+    print(fl_i)
+    i <- gsub(".png$", "", fl_i)
+    idx <- which(i == onco_meta_df[,datasets], arr.ind=TRUE)
+    
+    # Identity dataset and oncocode
+    ds <- datasets[idx[,2]]
+    oncocode <- as.character(gsub(":.*", "", onco_meta_df[idx[,1],]$oncocode))
+    
+    # Copy file to new locations
+    if(length(ds) > 0 & length(oncocode) > 0){
+      target_dir = file.path("output", sfc, cntype, ds, oncocode)
+    } else {
+      target_dir = file.path("output", sfc, cntype, "unknown")
+    }
+    if(!dir.exists(target_dir)) dir.create(target_dir, recursive = TRUE)
+    file.copy(file.path("output", sfc, cntype, fl_i),
+              file.path(target_dir, fl_i), overwrite = TRUE)
+  })
+} else if(analysis=='TCGA'){
   
-  # Copy file to new locations
-  if(length(ds) > 0 & length(oncocode) > 0){
-    target_dir = file.path(PDIR, "output", "hilbert", ds, oncocode)
-  } else {
-    target_dir = file.path(PDIR, "output", "hilbert", "unknown")
-  }
-  if(!dir.exists(target_dir)) dir.create(target_dir, recursive = TRUE)
-  file.copy(file.path(PDIR, "output", "hilbert", fl_i),
-            file.path(target_dir, fl_i), overwrite = TRUE)
-})
-
-
-
+}
+  
