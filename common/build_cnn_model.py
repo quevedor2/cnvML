403a4448019198c6fd686a28840f6ae964742cdd
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-04-18 14:03:54 -0400

    fixed indentations

--- common/build_cnn_model.py
+++ common/build_cnn_model.py
@@ -16,32 +16,33 @@ def main(argv):
     CNTYPE=sys.argv[4]        # TCN or ASCN
     DATASET=sys.argv[5]       # TCGA or CCL
     
-	try:
-		opts, args = getopt.getopt(argv,"hm:l:s:c:d:",["model=","lr=","sfc=","cntype=","dataset="])
-	except getopt.GetoptError:
-		print('build_cnn_model.py -m <model> -l <lr> -s <sfc> -c <cntype> -d <dir>')
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print('build_cnn_model.py -m <model> -l <lr> -s <sfc> -c <cntype> -d <dir>')
-			sys.exit()
+    try:
+        opts, args = getopt.getopt(argv,"hm:l:s:c:d:",["model=","lr=","sfc=","cntype=","dataset="])
+    except getopt.GetoptError:
+        print('build_cnn_model.py -m <model> -l <lr> -s <sfc> -c <cntype> -d <dir>')
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            print('build_cnn_model.py -m <model> -l <lr> -s <sfc> -c <cntype> -d <dir>')
+            sys.exit()
         elif opt in ("-m", "--model"):
-			model_type = arg
-		elif opt in ("-l", "--lr"):
-			lr = arg
-		elif opt in ("-s", "--sfc"):
-			SFC = arg
-		elif opt in ("-c", "--cntype"):
-			CNTYPE = arg
-		elif opt in ("-d", "--dataset"):
-			DATASET = arg
+            model_type = arg
+        elif opt in ("-l", "--lr"):
+            lr = arg
+        elif opt in ("-s", "--sfc"):
+            SFC = arg
+        elif opt in ("-c", "--cntype"):
+            CNTYPE = arg
+        elif opt in ("-d", "--dataset"):
+            DATASET = arg
     
     CATEGORIES = ["ACC", "BLCA", "BRCA", "CESC", "CHOL", "COAD",
-                  "DLBC", "ESCA", "GBM", "HNSC", "KICH", "KIRC",
-                  "KIRP", "LAML", "LGG", "LIHC", "LUAD", "LUSC",
-                  "MESO", "OV", "PAAD", "PCPG", "PRAD", "READ",
-                  "SARC", "SKCM", "STAD", "TGCT", "THCA", "THYM",
-                  "UCEC", "UCS", "UVM", "Normal"]
+        "DLBC", "ESCA", "GBM", "HNSC", "KICH", "KIRC",
+        "KIRP", "LAML", "LGG", "LIHC", "LUAD", "LUSC",
+        "MESO", "OV", "PAAD", "PCPG", "PRAD", "READ",
+        "SARC", "SKCM", "STAD", "TGCT", "THCA", "THYM",
+        "UCEC", "UCS", "UVM", "Normal"]
     
     (X, Xids, y) = load_data.readPickle(PDIR, DATASET, SFC, CNTYPE, CATEGORIES)
     
@@ -49,9 +50,9 @@ def main(argv):
     
     model_path = os.path.join(OUTDIR, model_type)
     M = model.buildModel(y, IMG_SIZE, lr, model_type, x_train, y_train_one_hot,
-                         x_test, y_test_one_hot, model_path)
+    x_test, y_test_one_hot, model_path)
     m_perf = anal.spotcheckModel(M, x_test, y_test_one_hot, CATEGORIES, model_path)
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
