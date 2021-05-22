f7675c9cb8dff4b02f6e380a4c6d57574c9cb286
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-05-22 11:19:24 -0400

    Added the new sfcs to the arrow map

--- R/arrowsSFC.R
+++ R/arrowsSFC.R
@@ -9,6 +9,7 @@
 ## or recursiveness
 
 library(GenomicRanges)
+library(RcnvML)
 PDIR <-'/mnt/work1/users/pughlab/projects/cancer_cell_lines/cnvML/hilbert_modeling'
 setwd(PDIR)
 
@@ -22,12 +23,16 @@ dir.create("arrows", recursive = F, showWarnings = F)
 sapply(c(4,6,8), function(ord){
   ord <- as.integer(ord)
   gbin_pos <- setupRefHcMatrix(order=ord)
-  bins <- mapSFC(sfc='sweep', hc_ord=gbin_pos$ord)
+  na <- lapply(c('sweep', 'scan', 'diagonal', 'morton', 'random'), function(sfc){
+    bins <- mapSFC(sfc=sfc, hc_ord=gbin_pos$ord)
+    
+    pdf(file.path("arrows", paste0(sfc, "_", ord, ".pdf")))
+    hc <- genHC(max(chr.size.dat$cum.end)/scale, ord, mode='normal',
+                sfc_pos=bins[-nrow(bins),c('x1', 'y1', 'x2', 'y2')])
+    dev.off()
+  })
   
-  pdf(file.path("arrows", paste0("sweep_", ord, ".pdf")))
-  hc <- genHC(max(chr.size.dat$cum.end)/scale, ord, mode='normal',
-              sfc_pos=bins[-nrow(bins),c('x1', 'y1', 'x2', 'y2')])
-  dev.off()
+  
   
   pdf(file.path("arrows", paste0("hilbert_", ord, ".pdf")))
   hc <- genHC(max(chr.size.dat$cum.end)/scale, ord, mode='normal')
